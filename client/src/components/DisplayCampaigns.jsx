edd52ced9f17b40fcc0bd791d969cc29dae661d2
Passakorn Klaikaew <passakornjiew@gmail.com>
2023-05-29 02:31:59 +0700

    Edit Style You will get

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -14,7 +14,7 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
 
   return (
     <div>
-      <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+      <h1 className="text-glow font-epilogue font-semibold text-[18px] text-white text-left">
         {title} ({campaigns.length})
       </h1>
 
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -82,13 +82,8 @@ const CreateCampaign = () => {
             handleChange={(e) => handleFormFieldChange("description", e)}
           />
 
-          <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[80px] rounded-[10px]">
-            <img
-              src={money}
-              alt="money"
-              className="w-[40px] h-[40px] object-contain"
-            />
-            <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">
+          <div className="btn-glow btn-gradient-border w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[80px] rounded-[10px]">
+            <h4 className="text-gradient font-epilogue font-bold text-[25px] text-white ml-[20px]">
               You will get 100% of the raised amount
             </h4>
           </div>
