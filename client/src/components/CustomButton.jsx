7914e076f1c13739e7029216b9a65944d253e891
stemG <77492060+gkana19@users.noreply.github.com>
2023-05-29 23:28:29 +0700

    [edit] responsive disconnect text

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -4,9 +4,8 @@ const CustomButton = ({ btnType, title, handleClick, styles }) => {
   return (
     <button
       type={btnType}
-      className={`btn-gradient-border btn-glow font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
-      onClick={handleClick}
-    >
+      className={`btn-gradient-border btn-glow font-epilogue font-semibold text-[12px] md:text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+      onClick={handleClick}>
       {title}
     </button>
   );
