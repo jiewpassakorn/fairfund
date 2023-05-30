9f1e0cf75cf0d459f14024b88a0fc62db5bdbe4a
stemG <77492060+gkana19@users.noreply.github.com>
2023-05-30 23:12:43 +0700

    edit ui

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -49,7 +49,7 @@ const DisplayCampaigns = ({ title, isLoading, campaigns, data }) => {
             </Tab>
           ))}
         </TabsHeader>
-        <TabsBody>
+        <TabsBody className="mt-2">
           {!isLoading && campaigns.length === 0 && (
             <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
               You have not created any campaigns yet
