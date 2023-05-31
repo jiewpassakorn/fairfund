0db47afe8967ae67ec47b37199793c1c758b6a88
Passakorn Klaikaew <passakornjiew@gmail.com>
2023-05-31 16:40:51 +0700

    edit path

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -12,7 +12,7 @@ import menu from "./menu.svg";
 import money from "./money.svg";
 import loader from "./loader.svg";
 import thirdweb from "./thirdweb.png";
-import fairfund from "./fairfund.svg";
+import fairfund from "./FairFund.svg";
 import newCampaign from "./file-new-icon.svg";
 import backIcon from "./back-icon.svg";
 
